c7436c64d7e63a6e934dac96b4c4bf9556a7604b
General-Airkay <generalairkay@gmail.com>
2026-06-05 11:43:25 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home() {
         {/* Background image */}
         <div style={{ position: 'absolute', inset: 0, zIndex: 0 }}>
           <Image
-            src="/photos/hero.jpg"
+            src="/public/hero.jpg"
             alt="Wedding background"
             fill
             style={{ objectFit: 'cover' }}
