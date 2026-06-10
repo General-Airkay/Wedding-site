266bd2f2eae8ef0b5c91acd14096ae0849c7de22
General-Airkay <generalairkay@gmail.com>
2026-06-10 15:11:07 +0100

    Update wedding-details.js

--- pages/wedding-details.js
+++ pages/wedding-details.js
@@ -151,7 +151,7 @@ export default function WeddingDetails() {
 
              <div style={{ textAlign: 'center', marginTop: '20px' }}>
             <a
-              href="https://drive.google.com"
+              href="https://www.dropbox.com/scl/fi/cthh9diz30vfzdungvifn/Wedding-Venue-Direction-Guide.jpg?rlkey=yyqqrbrw4fxdik1s806q3ncuk&st=zln23iy9&dl=1"
               target="_blank"
               rel="noopener noreferrer"
               className="btn direction-btn"
